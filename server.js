cd4331cc9e90cd986b80dad9dd66d254b351f882
pfsplcredit-oss <pfsplcredit@gmail.com>
2026-09-22 18:34:48 +0530

    Read eCourts page text

--- server.js
+++ server.js
@@ -145,14 +145,17 @@ const server = http.createServer(async (req, res) => {
         timeout: 30000
       });
 
-      const title = await page.title();
+const title = await page.title();
 const pageText = await page.locator("body").innerText();
-      await browser.close();
 
-      sendJson(res, 200, {
+await browser.close();
+
+sendJson(res, 200, {
   status: "success",
   message: "Playwright successfully opened eCourts",
   page_title: title,
+  page_text: pageText.substring(0, 3000)
+});
   page_text: pageText.substring(0, 3000)
 });
 
